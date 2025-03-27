545d678345e4943119ad4a8a7304dd01794aece8
Anish Kumar <anish.bplofficial@gmail.com>
2025-03-27 22:22:16 +0530

    impact section alligned correctly

--- newComponents/AboutSection.jsx
+++ newComponents/AboutSection.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import React from "react";
+import { MoveRight, ChevronRight } from "lucide-react";
 
 const AboutSection = () => {
   return (
-    <div className="flex justify-center items-center w-full h-[400px] p-4">
-      <div className="w-[900px] h-[200px]">
+    <div className="flex flex-col justify-center items-center w-full h-[400px] p-4">
+      <div className="py-3 px-10">
         <p className="text-lg text-center">
           Khudarpan is a passionate non-profitable organization dedicated to
           transforming lives across Bihar and Delhi through holistic social
@@ -24,6 +26,13 @@ const AboutSection = () => {
           their dreams and contribute meaningfully to national growth.
         </p>
       </div>
+      <Link
+        href="about/about-us"
+        className="text-[#007c7c] flex justify-center items-center"
+      >
+        <span className="font-bold text-lg">Read more about us</span>
+        <ChevronRight />
+      </Link>
     </div>
   );
 };
--- newComponents/ImpactSection.jsx
+++ newComponents/ImpactSection.jsx
@@ -5,27 +5,30 @@ const ImpactSection = () => {
   const impactData = [
     {
       id: 1,
-      impactNumber: 15,
-      impactName: "Lac Children",
-      impactDesc: "and their famlies are impacted every year",
+      impactNumber: "10K",
+      impactName: "People",
+      impactDesc: "and their famlies are benefitted and impacted every year",
     },
     {
       id: 2,
-      impactNumber: 2000,
-      impactName: "Lac Children",
-      impactDesc: "and their famlies are impacted every year",
+      impactNumber: 50,
+      impactName: "Villages",
+      impactDesc:
+        "are being reached out making ignificant inroads and touching communities ",
     },
     {
       id: 3,
-      impactNumber: 400,
-      impactName: "Lac Children",
-      impactDesc: "and their famlies are impacted every year",
+      impactNumber: 8,
+      impactName: "Projects",
+      impactDesc:
+        "focused on education, healthcare, women empowerment and on providing basic needs",
     },
     {
       id: 4,
-      impactNumber: 25,
-      impactName: "Lac Children",
-      impactDesc: "and their famlies are impacted every year",
+      impactNumber: 3,
+      impactName: "States",
+      impactDesc:
+        "and reached including the remotest area with all our support and team",
     },
   ];
 
@@ -38,11 +41,11 @@ const ImpactSection = () => {
           </h1>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8 ">
           {impactData.map((items) => (
             <div
               key={items.id}
-              className="flex flex-col items-center justify-center text-center p-4"
+              className="flex flex-col items-center justify-center text-center p-4 h-full"
             >
               <div className="flex items-center justify-center mb-2 text-[#007c7c]">
                 <span className="font-bold text-5xl md:text-6xl lg:text-7xl">
@@ -53,7 +56,7 @@ const ImpactSection = () => {
               <h3 className="font-semibold text-xl mb-2 text-gray-800">
                 {items.impactName}
               </h3>
-              <p className="text-gray-600 text-sm md:text-base">
+              <p className="text-gray-600 text-sm md:text-base flex-grow">
                 {items.impactDesc}
               </p>
             </div>
